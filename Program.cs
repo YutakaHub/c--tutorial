1013f732d48a884fa529eb5384bc579d7fab443c
YutakaHub <92905563+YutakaHub@users.noreply.github.com>
2023-10-21 14:27:25 +0900

    クラス・アクセス修飾子・コンストラクタ・静的メンバ等について学習。

--- Program.cs
+++ Program.cs
@@ -8,22 +8,25 @@ namespace Exercise601
 {
   class Program
   {
+    //ベクトルの内容を表示するメソッド。静的メンバ（Static）、インスタンスメンバについて学習。
+    static void Show(string name, Vector2D v)
+    {
+      Console.WriteLine("{0}={1},{2}", name, v.X, v.Y);
+    }
     static void Main(string[] args)
     {
-      Vector2D v1 = new Vector2D();
-      v1.X = 1.0; v1.Y = 1.0;
-      Console.WriteLine("v1={0},{1}", v1.X, v1.Y);
-
-      Vector2D v2 = new Vector2D();
+      Vector2D v1 = new Vector2D(1.0, 1.0);
+      Show("v1", v1);
+      Vector2D w = new Vector2D(1.0, -1.0);
+      Vector2D v2 = new Vector2D(w);
       v2.X = 1.0; v2.Y = -1.0;
-      Console.WriteLine("v2={0},{1}", v2.X, v2.Y);
-
+      Show("v2", v2);
       v1.Add(v2);
-      Console.WriteLine("v1={0},{1}", v1.X, v1.Y);
+      Show("v1", v1);
       v1.Sub(v2);
-      Console.WriteLine("v1={0},{1}", v1.X, v1.Y);
+      Show("v1", v1);
       v1.Mul(2.0);
-      Console.WriteLine("v1={0},{1}", v1.X, v1.Y);
+      Show("v1", v1);
 
       Console.WriteLine("v1=,v2の内積：{0}", v1.DotProduct(v2));
 
--- Vector2D.cs
+++ Vector2D.cs
@@ -8,28 +8,37 @@ namespace Exercise601
 {
   class Vector2D
   {
-    private double y;
+    //コンストラクタの生成
+    public Vector2D()
+    { }
+    public Vector2D(double x, double y)
+    {
+      X = x; Y = y;
+    }
+    public Vector2D(Vector2D v)
+    {
+      X = v.X; Y = v.Y;
+    }
     public void Add(Vector2D v)
     {
-      X += v.X; Y += v.y;
+      X += v.X; Y += v.Y;
     }
     public void Sub(Vector2D v)
     {
-      X -= v.X; y -= v.y;
+      X -= v.X; Y -= v.Y;
     }
     public void Mul(double k)
     {
-      X *= k; y *= k;
+      X *= k; Y *= k;
     }
     public double DotProduct(Vector2D v)
     {
-      return X * v.X + y * v.y;
+      return X * v.X + Y * v.Y;
     }
     //アクセス就職子について学習。プロパティを設定する。自動実装プロパティについても学習。
     public double Y
     {
-      set { y = value; }
-      get { return y; }
+      set; get;
     }
     public double X
     {
